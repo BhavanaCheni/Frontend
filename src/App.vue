1b72381969c3b044a0e286d20f0fe74eb19d4ee8
Bhavana Cheni <chbhavana48@gmail.com>
2022-05-08 23:28:04 -0400

    front end

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,104 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <!-- #3 must use the component-->
+    <Myheader mytitle="Pet Dogs" 
+      aboutpage ="Dog Breeds" />
+    
+    <Dogbox :dogs = "dogs"/>
+    <About aboutme="About" />
+    
+
+
+
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+
+//import HelloWorld from './components/HelloWorld.vue'
+// 1
+import Myheader from './components/Myheader.vue'
+import About from './components/About.vue'
+// ----import Bookbox from './components/Bookbox.vue'
+import Dogbox from './components/Dogbox.vue'
+
+
+// data url: http://localhost:5555/books , this might be different for you
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // 2 register the component 
+    Myheader,
+    Dogbox,
+    About
+  },
+
+  data(){
+    return {
+      dogs: []
+    }
+  },
+  methods: {
+    // promises
+
+      async fetchDogs(){
+         const res= await fetch('https://pet-dogs.herokuapp.com/db.json')
+         const data= await res.json()
+          console.log(data)
+         return data
+      }
+
+  },
+
+  async created(){
+    this.dogs = await this.fetchDogs()
   }
 }
 </script>
 
+
+
+
+
+
+
+
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+
+
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  background-color: #304D6D;
+}
+.container {
+  max-width: 400px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 0.3em solid black;
+  padding: 30px;
+  border-radius: 5px;
+  background-color: #A7CCED;
+}
+
+div{
+  margin-bottom: 0.5em;
+}
+
+
+
 </style>
+
+
---
+++ src/components/Dog.vue
@@ -0,0 +1,41 @@
+<template>
+   
+    <div>
+       <h2>{{dog.dogbreed}}</h2>
+       <h4>{{dog.behavoiur}}</h4>
+       <h4>{{dog.description}}</h4>
+       <h4>{{dog.breedcost}}</h4>
+       <h4>{{dog.breedlifetime}} </h4>
+    </div>
+   
+   
+   
+</template>
+
+<script>
+
+export default{
+    name: 'Dog',
+    props:{
+        dog: Array
+    }
+   
+}
+</script>
+
+<style scoped>
+
+h4{
+    margin: 2px;
+}
+
+
+div{
+    background-color: #63ADF2;
+    color: #003566;
+    padding: 1em;
+}
+
+
+
+</style>
